82e632879943977f50cb0170f0ee86644ba9d8a6
ssperandio <dev.ssperandio@gmail.com>
2023-09-06 12:34:04 -0300

    Introdução e estruturas condicionais com Java

---
+++ src/ResultadoEscolar.java
@@ -0,0 +1,32 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        // Condicional composta:
+        /* int nota = 6;
+
+        if(nota >= 7)
+            System.out.println("Aprovado");
+
+        else
+            System.out.println("Reprovado"); */
+
+
+        // Condicional encadeadas:
+        /* int nota = 6;
+
+        if(nota >= 7)
+        System.out.println("Aprovado");
+        else if(nota >= 5 && nota < 7)
+        System.out.println("Prova Recuperação");
+        else
+        System.out.println("Reprovado"); */
+        
+        // Condição ternária
+        int nota = 5;
+        /* String resultado = nota >= 7 ? "Aprovado" : "reprovado";
+
+        System.out.println(resultado); */
+
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota <7 ? "Recuperação" : "Reprovado";
+        System.out.println(resultado);
+    }
+}
